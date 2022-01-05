3e9bc7cd50f8ac596cf4e064467629bada855352
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-01-05 19:20:57 +0100

    PrivateRoute redirects to children with isLoggedIn true in props

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
         <Route
           path={"/profile"}
           element={
-            <PrivateRoute>
+            <PrivateRoute isLoggedIn={true}>
               <ProfileComponent />
             </PrivateRoute>
           }
