8a6899135fca81f83c2fb74295f5d09566f5c8a2
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-01-05 19:00:05 +0100

    console log the location in the PrivateRoute Wrapper

--- src/utils/PrivateRoute.tsx
+++ src/utils/PrivateRoute.tsx
@@ -1,4 +1,7 @@
+import { useLocation } from "react-router-dom";
+
 const PrivateRoute = (props: any) => {
+  console.log(useLocation()); // we have access to the location before we return something
   console.log(props);
 
   return props.children;
