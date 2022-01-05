2d7a9e0bb042af724617a93c6c0ec6ba2af65a9e
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-01-05 19:19:34 +0100

    PrivateRoute redirects to login if no isLoggedIn prop is found

--- src/utils/PrivateRoute.tsx
+++ src/utils/PrivateRoute.tsx
@@ -1,10 +1,13 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, Navigate } from "react-router-dom";
 
 const PrivateRoute = (props: any) => {
   console.log(useLocation()); // we have access to the location before we return something
   console.log(props);
-
-  return props.children;
+  if (props.isLoggedIn) {
+    return props.children;
+  } else {
+    return <Navigate to={"/login"} />;
+  }
 };
 
 export default PrivateRoute;
