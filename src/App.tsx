48db3daeff328753cfdf65e16744163921d11787
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-01-06 01:49:25 +0100

    Added nav and login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import PrivateRoute from "./utils/PrivateRoute";
 import HomeComponent from "./components/HomeComponent";
@@ -8,15 +9,20 @@ import LoginComponent from "./components/LoginComponent";
 import "./App.css";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <BrowserRouter>
+      <Link to="/">Home</Link> <Link to="/profile">Profile</Link>
       <Routes>
         <Route path={"/home"} element={<HomeComponent />} />
-        <Route path={"/login"} element={<LoginComponent />} />
+        <Route
+          path={"/login"}
+          element={<LoginComponent handleLogin={setIsLoggedIn} />}
+        />
         <Route
           path={"/profile"}
           element={
-            <PrivateRoute isLoggedIn={true}>
+            <PrivateRoute isLoggedIn={isLoggedIn}>
               <ProfileComponent />
             </PrivateRoute>
           }
--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -1,3 +1,16 @@
-const LoginComponent = () => <p>Here we will login</p>;
+import { useEffect } from "react";
+
+interface LoginProps {
+  handleLogin: (newLoginStatus: boolean) => void;
+}
+
+const LoginComponent = ({ handleLogin }: LoginProps) => {
+  return (
+    <>
+      <p>Here we will login</p>
+      <button onClick={() => handleLogin(true)}>Log me in</button>
+    </>
+  );
+};
 
 export default LoginComponent;
